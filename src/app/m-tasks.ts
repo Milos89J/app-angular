f290df42bbcdd8e99a123fa3a0eaee8c36623b94
Milos Jeremic <Milos.Jeremic@bdkadvokati.com>
2023-04-07 15:11:53 +0200

    first-page

---
+++ src/app/m-tasks.ts
@@ -0,0 +1,23 @@
+import {Task} from './Task'
+
+export const TASKS: Task[] = [
+    {
+        id: 1,
+        text: 'Task One',
+        day: '07.17.1989.',
+        reminder: true,
+    },
+    {
+        id: 2,
+        text: 'Task Two',
+        day: '02.13.1999.',
+        reminder: true,
+    },
+    {
+        id: 3,
+        text: 'Task Three',
+        day: '01.20.2000.',
+        reminder: false,
+    },
+    
+];
